0f853a9316fe0e7b1188cf2032c78d1f9d6d3795
Camilo Caroso <camilitosamuel@hotmail.com>
2020-03-02 19:06:17 -0500

    edit

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Template from "./pages/Template";
+import Main from "./pages/Main";
 import Login from "./pages/Login";
-// import Detail from "./pages/Detail";
-// import NoMatch from "./pages/NoMatch";
-// import Nav from "./components/Nav";
 
 function App() {
   return (
     <Router>
       <div>
-        {/* <Nav /> */}
         <Switch>
-          <Route exact path="/" component={Template} />
+          <Route exact path="/" component={Main} />
           <Route exact path="/login" component={Login} />
-          {/* <Route exact path="/books/:id" component={Detail} /> */}
-          {/* <Route component={NoMatch} /> */} */}
         </Switch>
       </div>
     </Router>
