d17a1bea2ae9c1584f480625283eff1c8337e7b9
Monica Scroggs <scrmr0210@gmail.com>
2020-03-03 21:26:25 -0500

    created navbar for landing page

---
+++ client/src/components/Container/index.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+
+function Container() {
+    return (
+
+        <body>
+            <div class="container">
+
+            </div>
+        </body>
+    )
+};
+
+export default Container;
