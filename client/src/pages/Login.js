06ec169a7e12188eb40227a04a42b2f762fb7e2b
Leyla Hussein <leylahssn2007@gmail.com>
2020-03-09 22:50:36 -0400

    update login

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,27 +1,31 @@
-import React from "react"
+import React, { Component } from "react"
+import axios from "axios"
+import { Link } from "react-router-dom"
+
+
 
 
 class Login extends Component {
-    state ={
+    state = {
         userName: "",
-        email:"",
-        password:""
+        email: "",
+        password: ""
     }
 
-    handleChange = (e)=>{
-        const {name, value} = e.target
+    handleChange = (e) => {
+        const { name, value } = e.target
         console.log(name, value)
-        this.setState({[name]: value})
+        this.setState({ [name]: value })
 
     }
 
-    
-    handleLogin = ()=>{
-        const {userName, email, password} = this.state
-        // const payload ={userName: userName, email: email, password:password}
-        const payload ={userName, email, password}
 
-        axios.post("/ourLoginApi", payload).then((res)=>{
+    handleLogin = () => {
+        const { userName, email, password } = this.state
+        // const payload ={userName: userName, email: email, password:password}
+        const payload = { userName, email, password }
+
+        axios.post("/ourLoginApi", payload).then((res) => {
             console.log(res)
         })
     }
@@ -37,58 +41,58 @@ class Login extends Component {
     handleInputChange = event => {
         const { name, value } = event.target;
         this.setState({
-          [name]: value
+            [name]: value
         });
     };
 
 
     render() {
         return (
+            <div id="loginContainer">
+                <div className="container"  style={{ fontSize: '15px' }}>
+                    <Link to="/">Main</Link>
 
-            <div className="container" id="loginContainer" style={{background:"white", fontSize: '15px'}}>
-            <Link to="/">Main</Link>
-
-                <div className="row">
-                    <div className="col m6">
-                        <h2 className="center-align">Login</h2>
-                        <div className="row">
-                            <form className="col s12">
-
+                    <div  className="row">
+                        <div id="loginForm" className="col m4">
+                            <h2 className="center-align">Login</h2>
                             <div className="row">
-                                    <div className="input-field col s12">
-                        
-                                        <input id="username" type="text" className="validate" name='userName' placeholder="Username" value={this.state.userName}  onChange={this.handleChange}></input>
-                                    
-                                    </div>
-                                </div>
+                                <form className="col s6">
 
-                                <div className="row">
-                                    <div className="input-field col s12">
-                                        <input id="email" type="email" className="validate" name='email' placeholder="email" value={this.state.email} onChange={this.handleChange}></input>
-                                        
+                                    <div className="row">
+                                        <div className="input-field col s12">
+
+                                            <input id="username" type="text" className="validate" name='userName' placeholder="Username" value={this.state.userName} onChange={this.handleChange}></input>
+
+                                        </div>
                                     </div>
-                                </div>
-                                <div className="row">
-                                    <div className="input-field col s12">
-                                        <input id="pass" type="password" className="validate" name='password' placeholder= "password" value={this.state.password} onChange={this.handleChange}></input>
-                                        
+
+                                    <div className="row">
+                                        <div className="input-field col s12">
+                                            <input id="email" type="email" className="validate" name='email' placeholder="email" value={this.state.email} onChange={this.handleChange}></input>
+
+                                        </div>
                                     </div>
-                                </div>
-                                
-                                <div className="divider"></div>
-                                <div className="row">
-                                    <div className="col m12">
-                                        <p className="right-align">
-                                            <button className="btn btn-large waves-effect waves-light" type="button" name="action">Login</button>
-                                        </p>
+                                    <div className="row">
+                                        <div className="input-field col s12">
+                                            <input id="pass" type="password" className="validate" name='password' placeholder="password" value={this.state.password} onChange={this.handleChange}></input>
+
+                                        </div>
                                     </div>
-                                </div>
-                            </form>
+
+                                    <div className="divider"></div>
+                                    <div className="row">
+                                        <div className="col m12">
+                                            <p className="right-align">
+                                                <button className="btn btn-large waves-effect waves-light" type="button" name="action">Login</button>
+                                            </p>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-
         )
     }
 }
