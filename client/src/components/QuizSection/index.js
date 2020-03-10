1ee6064fd5be7b76ade80ff0a9073976c5dbb77c
Manuel Corona <coronamvirgo@gmail.com>
2020-03-09 20:05:44 -0400

    user page's logo in place

--- client/src/components/QuizSection/index.js
+++ client/src/components/QuizSection/index.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import 'materialize-css/dist/css/materialize.min.css';
 import './style.css';
-import M from "materialize-css";
 import BackgroundImg from "../ParallaxTemp/assets/web-4841858_1280.jpg";
 
 
--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -1,17 +1,19 @@
-import React from "react"
-import Container from "../components/Container"
-import Navbar from "../components/Navbar"
+import React from "react";
+import Container from "../components/Container";
+import Navbar from "../components/Navbar";
 import ParallaxTemp from "../components/ParallaxTemp"
+
 
 function Main() {
     return (
+        <>
         <div className="grey lighten-1">
             <Container />
             <Navbar />
             <ParallaxTemp />
-
+            
         </div>
-
+</>
 
     )
 }
