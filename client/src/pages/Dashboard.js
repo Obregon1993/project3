2e1ea9cb8b2402240231d9e5e02d4f91ace701cb
Obregon1993 <cjangie13@gmail.com>
2020-03-12 14:23:00 -0400

    commends

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -174,6 +174,7 @@ class Dashboard extends Component {
         API.startQuiz({
             quiz: this.state.whatQuiz
         }).then(res =>{
+            //
             this.setState({
                 question1: res.data[0].question,
                 answer11: res.data[0].option1,
