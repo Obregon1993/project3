6e8efdb090a012c883f079d91c1ab120aee0557e
Monica Scroggs <scrmr0210@gmail.com>
2020-03-16 19:23:24 -0400

    added carousel to profile page, fixing styling

--- client/src/components/QuizSection/index.js
+++ client/src/components/QuizSection/index.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import 'materialize-css/dist/css/materialize.min.css';
 import './style.css';
 import BackgroundImg from "../ParallaxTemp/assets/web-4841858_1280.jpg";
+import Carousel from "../Carousel";
 
 
 
@@ -15,7 +16,10 @@ class QuizSection extends Component {
         return(
 
 <>
-<img className="background-img" src={BackgroundImg} alt="DEF"></img>
+<div><img className="background-img" src={BackgroundImg} alt="DEF"></img>
+<Carousel/>
+</div>
+
     
     </>
         
