9eb23d32ae92ae02b14b95e89ba7764ca119b15b
Camilo Caroso <camilitosamuel@hotmail.com>
2020-03-18 18:17:53 -0400

    buildFolder

---
+++ client/build/precache-manifest.79c431694046b1bfef8b981e8bde83a4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "d51abaa4f0e1b980eb3954c421d80097",
+    "url": "/static/media/red_background.d51abaa4.png"
+  },
+  {
+    "revision": "ebfba78eb236e3a8a2b6edcecf3a3d70",
+    "url": "/static/media/matrix-code-animation-gif-free-animated-background.ebfba78e.gif"
+  },
+  {
+    "revision": "b091f4aef6976360fa6d4a64b4aad352",
+    "url": "/static/media/binary_background.b091f4ae.jpg"
+  },
+  {
+    "revision": "d8223d430a8c00d7961e1f1dd421b359",
+    "url": "/static/media/Questioning_Skills-512.d8223d43.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "894d9c0fb1d9e6aad09b",
+    "url": "/static/js/main.639b339c.chunk.js"
+  },
+  {
+    "revision": "6cb43998863feecb62c8",
+    "url": "/static/js/2.83f2c836.chunk.js"
+  },
+  {
+    "revision": "894d9c0fb1d9e6aad09b",
+    "url": "/static/css/main.7d3a7980.chunk.css"
+  },
+  {
+    "revision": "6cb43998863feecb62c8",
+    "url": "/static/css/2.a4572ee7.chunk.css"
+  },
+  {
+    "revision": "ecddfcdd1ee5730e26388f3e39f0b50d",
+    "url": "/index.html"
+  }
+];
